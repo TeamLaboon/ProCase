23b25fb7ddd4c21a8ae09b6b831273ff3ee7f95b
Handika Saputra <putra.dika94@ymail.com>
2015-06-17 17:12:29 +0700

    InApp Browser View - WebView 1.0
    menampilkan prototype pada webview

--- app/src/main/java/com/flipbox/skyline/procase/Activity/ProjectDescription.java
+++ app/src/main/java/com/flipbox/skyline/procase/Activity/ProjectDescription.java
@@ -2,6 +2,7 @@ package com.flipbox.skyline.procase.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -15,18 +16,33 @@ public class ProjectDescription extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_description);
-        Bundle intent = getIntent().getExtras();
-        String name = intent.getString(ProjectList.EXTRA_MESSAGE_NAME);
-        String desc = intent.getString(ProjectList.EXTRA_MESSAGE_DESC);
-        TextView textName = (TextView)findViewById(R.id.textView6);
-        TextView textDesc = (TextView)findViewById(R.id.textView7);
-        textName.setText(name);
-        textDesc.setText(desc);
+     //   Bundle intent = getIntent().getExtras();
+      //  String name = intent.getString(ProjectList.EXTRA_MESSAGE_NAME);
+       // String desc = intent.getString(ProjectList.EXTRA_MESSAGE_DESC);
+      //  TextView textName = (TextView)findViewById(R.id.textView6);
+      //  TextView textDesc = (TextView)findViewById(R.id.textView7);
+      //  textName.setText(name);
+     //   textDesc.setText(desc);
         WebView myWebView = (WebView)findViewById(R.id.webView);
-        myWebView.loadUrl("https://m.marvelapp.com/");
-        myWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+      //  WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+       // webView.setWebViewClient(webViewClient);
+       // MyWebViewClient webViewClient = new MyWebViewClient(this);
+
+        myWebView.loadUrl("https://m.marvelapp.com/b64a10#5494936");
+        myWebView.setWebViewClient(new MyWebViewClient());
+      //  myWebView.setWebViewClient(new WebViewClient());
     }
 
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            return false;
+        }
+    }
 /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
